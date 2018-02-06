a68a4e6e72a8c0179e73bbf107112cc0f464a0e3
Mark Filter <markfilter@outlook.com>
2018-02-05 21:41:20 -0500

    Stable - Server spins up. Hello World at route hello

--- course-ideas/src/main/java/com/markzfilter/courses/Main.java
+++ course-ideas/src/main/java/com/markzfilter/courses/Main.java
@@ -1,7 +1,14 @@
 package com.markzfilter.courses;
 
+import static spark.Spark.*;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        get("/", (req, res) -> "<h1>Welcome Students!</h1>");
+
+        get("/hello", (req, res) -> "<h1>Hello World</h1>");
     }
+
 }
